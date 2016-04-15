ccfb1c89cf25738758f99e143657fa55fa10f229
Gio Beridze <giobero999@gmail.com>
2016-04-15 15:05:26 +0400

    finished

--- js/jquery.app.js
+++ js/jquery.app.js
@@ -40,7 +40,98 @@ jQuery(document).ready(function () {
         }
     );
     wow.init();
-});
+
+    var a = [];
+    a[0] = "images/icons/scala.svg";
+    a[1] = "images/icons/scala-black.svg";
+
+    a[2] = "images/icons/akka-black.png";
+    a[3] = "images/icons/akka.png";
+
+    a[4] = "images/icons/bootstrap.svg";
+    a[5] = "images/icons/bootstrap-black.svg";
+
+    a[7] = "images/icons/android-black.svg";
+    a[8] = "images/icons/android.svg";
+
+    a[9] = "images/icons/react.svg";
+    a[10] = "images/icons/react-black.svg";
+
+    a[11] = "images/icons/js-black.png";
+    a[12] = "images/icons/js.png";
+
+    a[13] = "images/icons/ios-black.png";
+    a[14] = "images/icons/ios.png";
+
+    a[15] = "images/icons/couchbase-black.svg";
+    a[16] = "images/icons/couchbase.svg";
+
+    a[17] = "images/icons/play-black.png";
+    a[18] = "images/icons/play.png";
+
+
+
+    $(document).ready(function() {
+      $('#scala').hover(function() {
+        $(this).attr('src',a[0]);
+      },function() {
+        $(this).attr('src',a[1]);
+      });
+      $('#akka').hover(function() {
+        $(this).attr('src',a[3]);
+      },function() {
+        $(this).attr('src',a[2]);
+      });
+      $('#bootstrap').hover(function() {
+        $(this).attr('src',a[4]);
+      },function() {
+        $(this).attr('src',a[5]);
+      });
+      $('#js').hover(function() {
+        $(this).attr('src',a[12]);
+      },function() {
+        $(this).attr('src',a[11]);
+      });
+      $('#android').hover(function() {
+        $(this).attr('src',a[8]);
+      },function() {
+        $(this).attr('src',a[7]);
+      });
+      $('#react').hover(function() {
+        $(this).attr('src',a[9]);
+      },function() {
+        $(this).attr('src',a[10]);
+      });
+      $('#ios').hover(function() {
+        $(this).attr('src',a[14]);
+      },function() {
+        $(this).attr('src',a[13]);
+      });
+      $('#couchbase').hover(function() {
+        $(this).attr('src',a[16]);
+      },function() {
+        $(this).attr('src',a[15]);
+      });
+      $('#play').hover(function() {
+        $(this).attr('src',a[18]);
+      },function() {
+        $(this).attr('src',a[17]);
+      });
+    });
+
+    $(".title-box a").click(function (){
+      $(".title-box a").removeClass("text-colored");
+      $(".join-us-title").removeClass("active");
+      $(this).addClass("text-colored");
+    });
+    $(".join-us-title").click(function (){
+      $(".join-us-title").removeClass("active");
+      $(".title-box a").removeClass("text-colored");
+
+      $(this).addClass("active");
+    });
+
+}); //end ready
 
 
 //sticky header on scroll
@@ -55,18 +146,19 @@ $(window).scroll(function(){
     } else {
         $('.back-to-top').fadeOut();
     }
-}); 
+});
 
 $('.back-to-top').click(function(){
     $("html, body").animate({ scrollTop: 0 }, 1000);
     return false;
 });
 
+
 /* ==============================================
 Contact App
 =============================================== */
 
-//var $ = jQuery.noConflict(); //Relinquish jQuery's control of the $ variable. 
+//var $ = jQuery.noConflict(); //Relinquish jQuery's control of the $ variable.
 
 /*global jQuery */
 jQuery(function ($) {
@@ -99,12 +191,13 @@ jQuery(function ($) {
                 this.$errorName.show(500);
                 this.$errorName.delay(4000);
                 this.$errorName.animate({
-                    height: 'toggle'  
+                    height: 'toggle'
                 }, 500, function() {
                     // Animation complete.
-                }); 
+                });
                 error = true; // change the error state to true
             }
+
 
             var email_compare = /^([a-z0-9_.-]+)@([da-z.-]+).([a-z.]{2,6})$/; // Syntax to compare against input
             var email = this.$contactFormEmail.val().toLowerCase(); // get the value of the input field
@@ -117,23 +210,23 @@ jQuery(function ($) {
                 this.$errorEmail.show(500);
                 this.$errorEmail.delay(4000);
                 this.$errorEmail.animate({
-                    height: 'toggle'  
+                    height: 'toggle'
                 }, 500, function() {
                     // Animation complete.
-                });         
+                });
                 error = true;
             }
 
             var message = this.$contactFormMessage.val(); // get the value of the input field
-            
-            if(message == "" || message == " " || message == "Message") {              
+
+            if(message == "" || message == " " || message == "Message") {
                 this.$errorMessage.show(500);
                 this.$errorMessage.delay(4000);
                 this.$errorMessage.animate({
-                    height: 'toggle'  
+                    height: 'toggle'
                 }, 500, function() {
                     // Animation complete.
-                });            
+                });
                 error = true; // change the error state to true
             }
 
@@ -141,10 +234,10 @@ jQuery(function ($) {
                 this.$errorForm.show(500);
                 this.$errorForm.delay(4000);
                 this.$errorForm.animate({
-                    height: 'toggle'  
+                    height: 'toggle'
                 }, 500, function() {
                     // Animation complete.
-                }); 
+                });
             }
 
             return error;
@@ -178,10 +271,10 @@ jQuery(function ($) {
                         $this.$confirmMessage.show(500);
                         $this.$confirmMessage.delay(4000);
                         $this.$confirmMessage.animate({
-                            height: 'toggle'  
+                            height: 'toggle'
                             }, 500, function() {
-                        });    
-                        
+                        });
+
                         $this.$contactFormName.val('');
                         $this.$contactFormEmail.val('');
                         $this.$contactFormMessage.val('');
@@ -202,9 +295,31 @@ jQuery(function ($) {
         }
     };
 
-    
+
 
     //Initializing the app
     ContactFormApp.init({});
 
 });
+
+
+    var inputs = document.querySelectorAll( '.inputfile' );
+    Array.prototype.forEach.call( inputs, function( input )
+    {
+    	var label	 = input.nextElementSibling,
+    		labelVal = label.innerHTML;
+
+    	input.addEventListener( 'change', function( e )
+    	{
+    		var fileName = '';
+    		if( this.files && this.files.length > 1 )
+    			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+    		else
+    			fileName = e.target.value.split( '\\' ).pop();
+
+    		if( fileName )
+    			label.querySelector( 'span' ).innerHTML = fileName;
+    		else
+    			label.innerHTML = labelVal;
+    	});
+    });
