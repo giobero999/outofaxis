f873bc245b911f18a0fce475e82e56fde4937ba0
Gio Beridze <giobero999@gmail.com>
2016-04-21 20:15:09 +0400

    site blog, join us

--- js/jquery.app.js
+++ js/jquery.app.js
@@ -11,11 +11,11 @@ Smooth Scroll To Anchor
 =============================================== */
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.navbar-nav a').bind('click', function(event) {
+    $('.navbar-nav a, .scroll-down-btn').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top - 0
-        }, 1500, 'easeInOutExpo');
+        }, 1500, 'jswing');
         event.preventDefault();
     });
 });
